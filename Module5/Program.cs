71c4f840256f18e142e86497e495841e09b02e5f
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-09-03 13:46:27 +0700

    Module5: reurning to favcolors

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -4,18 +4,37 @@
     {
         static void Main(string[] args)
         {
-            (string Name, string[] Dishes) User;
-            string[] Count = new[] {"first", "second", "third", "fourth", "fifth" };
+            (string Name, string[] Colors) User;
+            //string[] Count = new[] {"first", "second", "third", "fourth", "fifth" };
 
             Console.Write("What is your name: ");
             User.Name = Console.ReadLine();
             
-            User.Dishes = new string[5];
-            for (int i = 0; i < 5; i++)
+            static string[] ShowColor()
             {
-                Console.Write($"Enter your {Count[i]} favourite dish: ");
-                User.Dishes[i] = Console.ReadLine();
+                string[] Col = new string[3];
+                string[] Count = new[] { "first", "second", "third" };
+                for (int i = 0; i<3; i++)
+                {
+                    Console.Write($"Enter your {Count[i]} favourite color: ");
+                    Col[i] = Console.ReadLine();
+                }
+                return Col;
             }
+            
+            User.Colors = new string[3];
+            User.Colors = ShowColor();
+
+
+            Console.WriteLine(User.Name);
+            Console.WriteLine(User.Colors[0] + User.Colors[1]);
+
+            //User.Dishes = new string[5];
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write($"Enter your {Count[i]} favourite dish: ");
+            //    User.Dishes[i] = Console.ReadLine();
+            //}
 
         }
     }
