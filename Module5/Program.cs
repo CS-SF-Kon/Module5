4ee012ae76a0616599669cc135648644317601b3
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-09-03 13:19:07 +0700

    Module5: Unit 5.0 - introduction exercize

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            (string Name, string[] Dishes) User;
+            string[] Count = new[] {"first", "second", "third", "fourth", "fifth" };
+
+            Console.Write("What is your name: ");
+            User.Name = Console.ReadLine();
+            
+            User.Dishes = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter your {Count[i]} favourite dish: ");
+                User.Dishes[i] = Console.ReadLine();
+            }
+
         }
     }
 }
