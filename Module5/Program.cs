9416152c3617ff22c84d9219eb71ab6137d2f9db
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-09-04 16:09:26 +0700

    Module5: unit 5.5.3

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -2,48 +2,25 @@
 {
     internal class Program
     {
+        static void Echo(string phrase, byte depth)
+        {
+            string modif = phrase;
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+            }
+
+            Console.WriteLine("..." + modif);
+            if (depth > 1) Echo(modif, (byte) (depth - 1));
+        }
         static void Main(string[] args)
         {
-            (string Name, string[] Colors) User;
-            //string[] Count = new[] {"first", "second", "third", "fourth", "fifth" };
-
-            Console.Write("What is your name: ");
-            User.Name = Console.ReadLine();
+            Console.Write("Print something: ");
+            string text = Console.ReadLine();
+            Console.Write("Print the depth of echo: ");
+            byte depth = byte.Parse(Console.ReadLine());
             
-            static string[] ShowColor(string name)
-            {
-                string[] Col = new string[3];
-                string[] Count = new[] { "first", "second", "third" };
-                for (int i = 0; i<3; i++)
-                {
-                    Console.Write($"{name}, enter your {Count[i]} favourite color: ");
-                    Col[i] = Console.ReadLine();
-                }
-                return Col;
-            }
-            
-            User.Colors = new string[3];
-            User.Colors = ShowColor(User.Name);
-
-
-            //Console.WriteLine(User.Name);
-            //foreach (var color  in User.Colors) Console.WriteLine(color);
-
-            static void ShowCols (string name, params string[] favcolors)
-            {
-                Console.WriteLine(name);
-                foreach ( string col  in favcolors) Console.WriteLine(col);
-                
-            }
-
-            ShowCols(User.Name, User.Colors);
-
-            //User.Dishes = new string[5];
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.Write($"Enter your {Count[i]} favourite dish: ");
-            //    User.Dishes[i] = Console.ReadLine();
-            //}
+            Echo(text, depth);
 
         }
     }
